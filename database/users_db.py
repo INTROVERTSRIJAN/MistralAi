0cf5d11a2fc0e108b6b0ca5bbe64440173ad05b4
SRIJAN <141845601+INTROVERTSRIJAN@users.noreply.github.com>
2024-12-09 14:58:02 +0530

    Update users_db.py

--- database/users_db.py
+++ database/users_db.py
@@ -1,42 +1,44 @@
-#devggn
+#srijan
 
 from info import MONGO_URL
 from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 
-
+# Initialize MongoDB connection
 mongo = MongoCli(MONGO_URL)
-db = mongo.users
-db = db.users_db
+db = mongo.people  # Database remains the same, collection is now 'people'
 
+async def get_people():
+    """
+    Retrieve all people from the collection where 'person' field exists and is greater than 0.
+    """
+    people_list = []
+    async for person in db.find({"person": {"$gt": 0}}):
+        people_list.append(person['person'])
+    return people_list
 
-async def get_users():
-  user_list = []
-  async for user in db.users.find({"user": {"$gt": 0}}):
-    user_list.append(user['user'])
-  return user_list
+async def get_person(person):
+    """
+    Check if a person exists in the collection.
+    """
+    people = await get_people()
+    return person in people
 
+async def add_person(person):
+    """
+    Add a new person to the collection if they don't already exist.
+    """
+    people = await get_people()
+    if person in people:
+        return
+    else:
+        await db.insert_one({"person": person})
 
-async def get_user(user):
-  users = await get_users()
-  if user in users:
-    return True
-  else:
-    return False
-
-async def add_user(user):
-  users = await get_users()
-  if user in users:
-    return
-  else:
-    await db.users.insert_one({"user": user})
-
-
-async def del_user(user):
-  users = await get_users()
-  if not user in users:
-    return
-  else:
-    await db.users.delete_one({"user": user})
-    
-
-
+async def del_person(person):
+    """
+    Delete a person from the collection if they exist.
+    """
+    people = await get_people()
+    if person not in people:
+        return
+    else:
+        await db.delete_one({"person": person})
