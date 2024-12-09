4ae12368566c663b5e82c8b441c557e98c7424c7
SRIJAN <141845601+INTROVERTSRIJAN@users.noreply.github.com>
2024-12-09 15:16:31 +0530

    Update people_db.py

--- database/people_db.py
+++ database/people_db.py
@@ -5,14 +5,15 @@ from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 
 # Initialize MongoDB connection
 mongo = MongoCli(MONGO_URL)
-db = mongo.people  # Database remains the same, collection is now 'people'
+db = mongo.people  # Database name
+collection = db.people  # Collection name
 
 async def get_people():
     """
     Retrieve all people from the collection where 'person' field exists and is greater than 0.
     """
     people_list = []
-    async for person in db.find({"person": {"$gt": 0}}):
+    async for person in collection.find({"person": {"$gt": 0}}):
         people_list.append(person['person'])
     return people_list
 
@@ -20,25 +21,18 @@ async def get_person(person):
     """
     Check if a person exists in the collection.
     """
-    people = await get_people()
-    return person in people
+    return await collection.find_one({"person": person}) is not None
 
 async def add_person(person):
     """
     Add a new person to the collection if they don't already exist.
     """
-    people = await get_people()
-    if person in people:
-        return
-    else:
-        await db.insert_one({"person": person})
+    if not await get_person(person):
+        await collection.insert_one({"person": person})
 
 async def del_person(person):
     """
     Delete a person from the collection if they exist.
     """
-    people = await get_people()
-    if person not in people:
-        return
-    else:
-        await db.delete_one({"person": person})
+    if await get_person(person):
+        await collection.delete_one({"person": person})
